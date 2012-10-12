04457e8cd2923e02fc9e2941f7bd7943612a02fd
Simon Otter <skurmedel@gmail.com>
2012-10-12 17:24:35 +0300

    Renamed the creation function. License.

--- various/SO_geoSphere.py
+++ various/SO_geoSphere.py
@@ -1,7 +1,32 @@
+# Copyright (c) 2012, Simon Otter
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import pymel.core as pm
 import pymel.core.datatypes as dt
 
-def create_geosphere(quads=True, it=1):
+def create(quads=True, it=1):
 	name = pm.polyPlatonicSolid(n="geoSphere", st=1)
 	for i in xrange(0, it):
 		pm.polySmooth(name, mth=int(not quads), dv=1)
@@ -29,7 +54,7 @@ def prompt_ui():
 	quads_chbox = pm.checkBox(label="Create Quads?", v=False, parent=layout)
 
 	def on_ok(*args):
-		create_geosphere(quads_chbox.getValue(), iter_field.getValue())
+		create(quads_chbox.getValue(), iter_field.getValue())
 		win.delete()
 
 	ok_button = pm.button(label="Create mah Geosphere!", command=on_ok, parent=layout)
