59da470cee60a66892ffaa6136f118231ce0d24e
skurmedel <skurmedel@gmail.com>
2012-11-13 01:39:07 +0100

    attachInstance is somewhat useful now.

--- various/SO_meshAttach.py
+++ various/SO_meshAttach.py
@@ -5,20 +5,25 @@
 import pymel.core as pm
 
 
-def attachInstance(source, target):
-    """Instances source, and attaches it to mesh target."""
+def attachInstance(source, target, p):
+    """Instances source, and attaches it to mesh target as close
+    as it can on the surface to point p."""
     
-    # Rework this shit so it uses a parameter instead.
-    vertices = pm.filterExpand(sm=31)
-    
-    for v in vertices:
-        inst = pm.instance(source)
-        constraint = pm.pointOnPolyConstraint(target, inst)   
-    
-        pos = pm.xform(v, q=True, t=True)
-    
-        n = pm.PyNode(target)
-        uvs = n.getUVAtPoint(pos)
-    
-        constraint.attr("%sU0" % n.nodeName()).set(uvs[0])
-        constraint.attr("%sV0" % n.nodeName()).set(uvs[1])
+    n = pm.PyNode(target)
+    if n.nodeType() == "transform" and n.numChildren():
+    	n = n.getChildren()[0]
+    if n.nodeType() != "mesh":
+    	raise ValueError("target must point to a polygon mesh.")
+
+    inst = pm.instance(source)
+    constraint = pm.pointOnPolyConstraint(target, inst)
+
+    uvs = n.getUVAtPoint(p, space="world")
+
+    transform = n.firstParent()
+
+    constraint.attr("%sU0" % transform.nodeName()).set(uvs[0])
+    constraint.attr("%sV0" % transform.nodeName()).set(uvs[1])
+
+def raycastPosition():
+	return pm.autoPlace(useMouse=True)
