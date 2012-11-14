dc573a7683cc3a9d71d53f45f48881263c826cc9
skurmedel <skurmedel@gmail.com>
2012-11-15 00:57:09 +0100

    Added attachToVertices.

--- various/SO_meshAttach.py
+++ various/SO_meshAttach.py
@@ -4,16 +4,20 @@
 
 import pymel.core as pm
 
+def _getMeshNode(name):
+    n = pm.PyNode(name)
+    if n.nodeType() == "transform" and n.numChildren():
+        n = n.getChildren()[0]
+    if n.nodeType() != "mesh":
+        raise ValueError("name must point to a polygon mesh.")
+    return n
 
 def attachInstance(source, target, p):
     """Instances source, and attaches it to mesh target as close
-    as it can on the surface to point p."""
+    as it can on the surface to point p.
+    """
     
-    n = pm.PyNode(target)
-    if n.nodeType() == "transform" and n.numChildren():
-    	n = n.getChildren()[0]
-    if n.nodeType() != "mesh":
-    	raise ValueError("target must point to a polygon mesh.")
+    n = _getMeshNode(target)
 
     inst = pm.instance(source)
     constraint = pm.pointOnPolyConstraint(target, inst)
@@ -25,5 +29,25 @@ def attachInstance(source, target, p):
     constraint.attr("%sU0" % transform.nodeName()).set(uvs[0])
     constraint.attr("%sV0" % transform.nodeName()).set(uvs[1])
 
+def attachToVertices(source, targetVertices=None):
+    """Instances source, and attaches each instance to each
+    vertice.
+
+    If targetVertices == None, the selected vertices are used.
+    """
+
+    if not targetVertices:
+        targetVertices = pm.filterExpand(pm.ls(sl=True), expand=True, sm=31)
+    else:
+        if not hasattr("__next__", targetVertices):
+            raise TypeError("targetVertices is not iterable.")
+
+    for v in targetVertices:
+        p = pm.pointPosition(v, w=True)
+
+        target = pm.PyNode(v).node()
+
+        attachInstance(source, target, p)
+
 def raycastPosition():
-	return pm.autoPlace(useMouse=True)
+    return pm.autoPlace(useMouse=True)
